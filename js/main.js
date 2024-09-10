d1d45c49d89c02e4d487907e6971c2cd645250cf
hayayoshi06 <152338328+hayayoshi06@users.noreply.github.com>
2024-09-10 10:59:29 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,12 +2,39 @@
 
 {
   const ham = document.querySelector('.hamburger');
-  const nav = document.querySelector('#nav');
+  const navi = document.querySelector('#navi');
   const mask = document.querySelector('#mask');
 
   ham.addEventListener('click', function () {
     ham.classList.toggle('active');
-    nav.classList.toggle('active');
+    navi.classList.toggle('active');
     mask.classList.toggle('active');
   });
+
+  function scrollCallback (entries) {
+    entries.forEach(entry => {
+      if(!entry.isIntersecting) {
+        header.classList.add("scrolled");
+        toTop.classList.add("scrolled");
+      } else {
+        header.classList.remove("scrolled");
+        toTop.classList.remove("scrolled");
+      }
+    });
+  }
+
+  const toTop = document.getElementById("to_top");
+
+  toTop.addEventListener("click", e => {
+    e.preventDefault();
+    window.scrollTo({
+      top:0,
+      behavior:"smooth",
+    });
+  });
+
+  const header = document.querySelector("header");
+
+  const scrollObserver = new IntersectionObserver(scrollCallback);
+  scrollObserver.observe(document.getElementById("target-2"));
 }
